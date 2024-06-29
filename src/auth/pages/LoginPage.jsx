a06b7cc45986203126e1dc875d81742a7c293658
martinrdrz <martinrdrz@hotmail.com>
2024-06-29 11:49:30 -0300

    trabajando con formulario de autenticacion

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -3,8 +3,8 @@ import './LoginPage.css';
 export const LoginPage = () => {
     return (
         <div className='container login-container'>
-            <div className='row justify-content-center'>
-                <div className='col-md-5 login-form-1'>
+            <div className='row'>
+                <div className='col-md-6 login-form-1'>
                     <h3>Ingreso</h3>
                     <form>
                         <div className='form-group mb-2'>
@@ -13,13 +13,13 @@ export const LoginPage = () => {
                         <div className='form-group mb-2'>
                             <input type='password' className='form-control' placeholder='Contraseña' />
                         </div>
-                        <div className='form-group mb-2'>
+                        <div className='d-grid gap-2'>
                             <input type='submit' className='btnSubmit' value='Login' />
                         </div>
                     </form>
                 </div>
 
-                <div className='col-md-5 login-form-2'>
+                <div className='col-md-6 login-form-2'>
                     <h3>Registro</h3>
                     <form>
                         <div className='form-group mb-2'>
@@ -31,10 +31,12 @@ export const LoginPage = () => {
                         <div className='form-group mb-2'>
                             <input type='password' className='form-control' placeholder='Contraseña' />
                         </div>
+
                         <div className='form-group mb-2'>
                             <input type='password' className='form-control' placeholder='Repita la contraseña' />
                         </div>
-                        <div className='form-group mb-2'>
+
+                        <div className='d-grid gap-2'>
                             <input type='submit' className='btnSubmit' value='Crear cuenta' />
                         </div>
                     </form>
