fe5142836b5a489ca5e8490c18ca356828505483
martinrdrz <martinrdrz@hotmail.com>
2024-07-14 14:23:35 -0300

    aplicacion finalizada

--- src/store/auth/authSlice.js
+++ src/store/auth/authSlice.js
@@ -21,7 +21,7 @@ export const authSlice = createSlice({
             state.errorMessage = undefined;
         },
         onLogout: (state, { payload }) => {
-            state.status = 'not authenticated';
+            state.status = 'not-authenticated';
             state.user = {};
             state.errorMessage = payload;
         },
